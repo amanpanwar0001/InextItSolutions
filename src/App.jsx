8320ad52f4c9d4acf42eb66038e4d04eae7c8f6b
aman parmar <parmaraman.7089@gmail.com>
2024-09-17 14:23:52 +0530

    inext form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+ import PricingCalculator from './PricingCalculator'
+ import Fake from './Fake'
+ 
+ const App = () => {
+   return (
+     <div>
+      <PricingCalculator/>
+     </div>
+   )
+ }
+ 
+ export default App
